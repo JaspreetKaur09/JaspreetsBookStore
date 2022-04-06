b91773e9b5271960f1006afa43b0e94d04c45948
Jaspreet Kaur <Jaspreet Kaur@DAISY>
2022-04-06 13:27:34 -0500

    Add project files.

---
+++ JaspreetsBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using JaspreetsBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JaspreetsBookStore.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ JaspreetsBooks.Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace JaspreetsBooks.Utility
+{
+    public static class SD
+    {
+    }
+}
